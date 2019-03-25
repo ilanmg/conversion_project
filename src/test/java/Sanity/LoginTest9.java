fa0596430ee4b43286d60375eacceb4090d5af95
IlanMg <ilanmg@artlist.io>
2019-03-25 09:18:57 +0200

    finish

---
+++ src/test/java/Sanity/LoginTest9.java
@@ -0,0 +1,25 @@
+package Sanity;
+
+
+import LoginPages.LoginPage;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+public class LoginTest9 extends BaseTest{
+
+
+
+    @Test
+    public void LoginTest() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.goTo();
+        //driver.manage().window().maximize();
+        loginPage.signInBtn();
+        Thread.sleep(5000);
+        loginPage.enterUserCredentials("#logemail", "#logpassword");
+        loginPage.loginBtn();
+
+        //Assert.assertEquals(true, siteHeader.getLicenseBtn());
+
+    }
+}
