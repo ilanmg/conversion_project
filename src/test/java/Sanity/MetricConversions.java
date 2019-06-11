0af369b6c819ec4f11d67a1a20ad06c312e323bc
unknown <ilanmg@artlist.io>
2019-06-11 19:43:09 +0300

    first commit

--- src/test/java/Sanity/MetricConversions.java
+++ src/test/java/Sanity/MetricConversions.java
@@ -1,32 +1,26 @@
 package Sanity;
 
 
-import LoginPages.metricConversions;
 import Sanity.MetricConversions;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 public class MetricConversions{
 
    
-
+	
 	private char[] answer;
 
 	@Test
     public void CelsiusToFahrenheit() throws InterruptedException {
     	//metricConversions MetricConversions = new metricConversions(driver); 
-      
+		//System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
     	WebDriver driver = new ChromeDriver();
     	MetricConversions.goTo(); 
         driver.manage().window().maximize(); 
-    	
-        //Thread.sleep(5000);
-        MetricConversions.CelsiusField();
+    	MetricConversions.CelsiusField();
         MetricConversions.celsiusInput("44");
         MetricConversions.ConvertInput();
         MetricConversions.answerInput();
